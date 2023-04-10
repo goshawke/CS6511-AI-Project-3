0888fd5f0252fd2d910cddc16b701d606447c0e5
goshawke <52211392+goshawke@users.noreply.github.com>
2023-04-09 20:27:26 -0400

    test commit

---
+++ src/main/java/com/CS6511_AI/maven/tictactoe/App.java
@@ -0,0 +1,13 @@
+package com.CS6511_AI.maven.tictactoe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
